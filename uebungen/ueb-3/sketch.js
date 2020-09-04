8804acda335f88a647d3c2c170101158f199ae92
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-09-04 13:50:16 +0200

    index.html

--- uebungen/ueb-3/sketch.js
+++ uebungen/ueb-3/sketch.js
@@ -1,10 +1,57 @@
 function setup() {
-  createCanvas(320, 320);
-  background(200);
+  createCanvas(600, 400);
+
+  noStroke();
 }
 
 function draw() {
-  strokeWeight(1);
-  triangle(160, 0, 0, 320, 320, 320);
-  rect(80, 80, 160, 160);
+  background(240);
+
+  let a=0;
+
+  fill(0,100);
+  ellipse(a,height/2,90,90);
+
+  //kreis 1
+  a+=60;
+  //a=a+60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 2
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 3
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 4
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 5
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 6
+  a+=60;
+
+  ellipse(a,height/2,90,90);
+
+  //kreis 7
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 8
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 9
+  a+=60;
+  ellipse(a,height/2,90,90);
+
+  //kreis 10
+  a+=60;
+  ellipse(a,height/2,90,90);
+
 }
