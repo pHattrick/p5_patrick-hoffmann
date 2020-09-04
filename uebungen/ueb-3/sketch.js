c8aa4b6e8b9fb4e523780bbc48d097be7562fbd6
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-09-04 15:13:06 +0200

    no message

--- uebungen/ueb-3/sketch.js
+++ uebungen/ueb-3/sketch.js
@@ -8,50 +8,71 @@ function draw() {
   background(240);
 
   let a=0;
+  let abstand=60;
+  let grauwert=100;
 
-  fill(0,100);
+
+  fill(grauwert,100);
   ellipse(a,height/2,90,90);
 
   //kreis 1
-  a+=60;
-  //a=a+60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert,100)
   ellipse(a,height/2,90,90);
 
   //kreis 2
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 3
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 4
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 5
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 6
-  a+=60;
-
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 7
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 8
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 9
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
   //kreis 10
-  a+=60;
+  a+=abstand;
+  grauwert=grauwert*1.2;
+  fill(grauwert, 100)
   ellipse(a,height/2,90,90);
 
 }
---
+++ uebungen/ueb-4/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(320, 320);
+  background(200);
+}
+
+function draw() {
+  strokeWeight(5);
+  line(160, 50, 160, 270);
+  line(50, 160, 270, 160);
+}
