95e3bc33ed833bbd4a4ee77d5057f1d612c73371
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-09-18 21:20:49 +0200

    no message

--- uebungen/ueb-1/sketch.js
+++ uebungen/ueb-1/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-  createCanvas(320, 320);
-  background(200);
+createCanvas(320, 320);
+background(200);
 }
 
 function draw() {
-  strokeWeight(1);
-  triangle(160, 0, 0, 320, 320, 320);
-  rect(80, 80, 160, 160);
+strokeWeight(1);
+triangle(160, 0, 0, 320, 320, 320);
+
 }
