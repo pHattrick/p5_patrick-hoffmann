70339bb0703046a6b5d28fa7c3f00023518083c9
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-09-19 16:11:19 +0200

    no message

--- uebungen/ueb-for/sketch.js
+++ uebungen/ueb-for/sketch.js
@@ -1,51 +1,17 @@
-
-
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
- 
   background(0);
   stroke(255);
-  let x=10;
-  
-  line(x,0,x,height);
-  
-  x+=10;
-  //x=x+10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
-  x+=10;
-  line(x,0,x,height);
-  
- 
-  
+
+
+
+  for (let x = 0; x <= width; x += 10) {
+    line(x, 0, x, height);
+
+
+  }
+
 }
