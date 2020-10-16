183c83cfabdb6e99b8b1203e69a3e0d604c15595
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-10-16 23:16:57 +0200

    no message

---
+++ uebungen/ueb-4/sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(800, 800);
+  background(255);
+  strokeWeight(2);
+  stroke(0);
+  noFill();
+}
+
+function draw() {
+
+mouseX = background(0)
+  let stepsx = 40;
+  let stepsy = 100;
+  for (var x = 20; x < width; x += stepsx) {
+    for (var y = 120; y < height; y += stepsy) {
+
+      fill(200);
+      noStroke();
+      rect(100-80, 100, 200, 50, 30);
+      ellipse(140-80, 100, 50)
+      ellipse(170-80, 90, 50)
+      ellipse(200-80, 80, 50)
+      ellipse(230-80, 90, 50)
+      ellipse(260-80, 100, 50)
+
+      fill(50);
+      noStroke();
+      rect(100, 100, 200, 50, 30);
+      ellipse(140, 100, 50)
+      ellipse(170, 90, 50)
+      ellipse(200, 80, 50)
+      ellipse(230, 90, 50)
+      ellipse(260, 100, 50)
+
+
+      rect(x + 20, y + 40, 10, 40, 30);
+      rect(x + 40, y + 90, 10, 40, 30);
+
+
+
+
+    }
+  }
+}
