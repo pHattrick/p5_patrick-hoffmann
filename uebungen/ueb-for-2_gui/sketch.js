fb1c291de72b6311a1294818eaa61cf1095f369d
Patrick Hoffmann <patrick.hoffmann@gmail.com>
2020-10-20 22:20:32 +0200

    gui und ueb-4.4

--- uebungen/ueb-for-2_gui/sketch.js
+++ uebungen/ueb-for-2_gui/sketch.js
@@ -7,7 +7,7 @@ var distanz = 25;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   gui = createGui('p5.gui');
-gui.addGlobals('anz', 'distanz');
+  gui.addGlobals('anz', 'distanz');
   noFill();
   noLoop();
 }
@@ -16,7 +16,7 @@ function draw() {
   background(220);
   strokeWeight(12);
 
-  for (let x = 0; x <= anz ; x += 1) {
+  for (let x = 0; x <= anz; x += 1) {
     rect(distanz * x + distanz, distanz * x + distanz, 500, 500);
     console.log(x)
   }
